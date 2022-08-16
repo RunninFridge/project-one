a725c4e01b6e2aa0662af820d3e577243afa1863
Stephen Novelli <snovelli1021@gmail.com>
2022-08-15 21:15:20 -0700

    Updating new Navbar and CSS Styling added click functions to our buttons

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var youTubeBtn = document.getElementById("youTubeBtn");
+var appleMusicBtn = document.getElementById("appleMusicBtn");
+var spotifyBtn = document.getElementById("spotifyBtn");
+
+youTubeBtn.onclick = function() {
+    window.location.href ='https://youtube.com/';
+};
+
+appleMusicBtn.onclick = function() {
+    window.location.href ='https://music.apple.com/us/browse/';
+};
+
+spotifyBtn.onclick = function() {
+    window.location.href ='https://open.spotify.com/';
+};
