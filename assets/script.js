aff0fa8e5252646f11ed43e1a9a524a3f5377551
Vadim Merenovskiy <101852782+RunninFridge@users.noreply.github.com>
2022-08-18 10:12:44 -0700

    basic

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var youTubeBtn = document.getElementById("youTubeBtn");
 var appleMusicBtn = document.getElementById("appleMusicBtn");
 var spotifyBtn = document.getElementById("spotifyBtn");
-var Genius = require("genius-lyrics"); // for genius ?
-var client = new client("BOV6yaftuJuARA2ojkIunAabxrVWK-DmjLWuT5JHjy0UYAnqon9Cku0dY4E1YM0B"); //for genius ??
+
+
 
 //lets you open links in new tabs onclick - vm
 youTubeBtn.onclick = function() {
@@ -17,18 +17,4 @@ spotifyBtn.onclick = function() {
     window.open('https://open.spotify.com/');
 };
 
-
-
-var searches = client.songs.search("");
-
-// fetching songs from genius -vm
-var firstSong = searches[0];
-console.log("About the Song:\n", firstSong, "\n");
-
-// fetching lyrics from genius -vm
-var lyrics = firstSong.lyrics();
-console.log("Lyrics of the Song:\n", lyrics, "\n");
-
-// fetching an artist from genius -vm
-var artist = client.artists.get();
-console.log("About the Artist:\n", artist, "\n");
+https://api.musixmatch.com/ws/1.1/......?&apikey=QTbUwuc4VmQH4fCYBn2UmCYq0CzG9SG8O2VNVfvr
