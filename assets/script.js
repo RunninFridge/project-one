cd81eb5d0479cfa2c8a5825383bf2a88ec1aed62
Vadim Merenovskiy <101852782+RunninFridge@users.noreply.github.com>
2022-08-18 11:04:13 -0700

    searing lyrics by aritist?

--- assets/script.js
+++ assets/script.js
@@ -28,3 +28,85 @@ fetch(requestURL)
         console.log(data)
     })
 }
+
+
+
+function getLyrics() {
+    var artistSearch = document.getElementById("artistSearch").value;
+    document.getElementById("lyrics").textContent = "";
+    $.ajax({
+      type: "GET",
+      data: {
+        apikey: "QTbUwuc4VmQH4fCYBn2UmCYq0CzG9SG8O2VNVfvr",
+        q_artist: artistSearch,
+        format: "json",
+        callback: "json_callback"
+      },
+      url: "https://api.musixmatch.com/ws/1.1/track.search",
+      dataType: "json",
+      jsonCallback: "json_callback",
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        console.log(data.message.body.track_list[0].track.album_coverart_350x350);
+        console.log(data.message.body.track_list[0].track.lyrics_id);
+        var rand =
+          data.message.body.track_list[
+            Math.floor(Math.random() * data.message.body.track_list.length)
+          ];
+        console.log(rand.track.track_id);
+        var thisTrack = rand.track.track_id;
+        var thisPic = rand.track.album_coverart_350x350;
+        console.log(thisPic);
+  
+        var p = document.createElement("p");
+        p.textContent = thisTrack;
+        p.id = thisTrack;
+  
+        document.getElementById("lyrics").appendChild(p).style.opacity = 0;
+        document.getElementById("ghost").click();
+      },
+      error: function (jqXHR, textStatus, errorTh) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(errorTh);
+      }
+    });
+  }
+  
+  function getLyricsNow() {
+    var trackId = document.getElementById("lyrics").textContent;
+    console.log(trackId);
+    $.ajax({
+      type: "GET",
+      data: {
+        apikey: "QTbUwuc4VmQH4fCYBn2UmCYq0CzG9SG8O2VNVfvr",
+        track_id: trackId,
+        format: "json",
+        callback: "json_callback"
+      },
+      url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get",
+      dataType: "json",
+      jsonCallback: "json_callback",
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+        console.log(data.message.body.lyrics.lyrics_body);
+        var lyricsBody =
+          data.message.body.lyrics.lyrics_body
+            .split(/\s+/)
+            .slice(0, 100)
+            .join(" ") + "...";
+  
+        var j = document.createElement("p");
+        j.textContent = lyricsBody;
+        document.getElementById("lyrics").appendChild(j);
+      },
+      error: function (jqXHR, textStatus, errorTh) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(errorTh);
+      }
+    });
+  }
+  
