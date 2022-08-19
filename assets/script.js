cb930511cbee2391dc666551dab1155f1b1fabab
Stephen Novelli <snovelli1021@gmail.com>
2022-08-18 19:33:31 -0700

    Made changes to HTML id and class names, added JavaScript to return song lyrics

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,10 @@
-es (94 sloc)  3.14 KB
-
-var youTubeBtn = document.getElementById("youTubeBtn");
-var appleMusicBtn = document.getElementById("appleMusicBtn");
-var spotifyBtn = document.getElementById("spotifyBtn");
-
+var youtubeBtn = document.getElementById("youtube-btn");
+var appleMusicBtn = document.getElementById("apple-music-btn");
+var spotifyBtn = document.getElementById("spotify-btn");
 
 
 //lets you open links in new tabs onclick - vm
-youTubeBtn.onclick = function() {
+youtubeBtn.onclick = function() {
     window.open('https://youtube.com/');
 };
 
@@ -21,92 +18,10 @@ spotifyBtn.onclick = function() {
 
 
 
-function getApi() {
-    var requestURL = 'https://api.musixmatch.com/ws/1.1/?&apikey=QTbUwuc4VmQH4fCYBn2UmCYq0CzG9SG8O2VNVfvr'
-
-fetch(requestURL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data)
-    })
-}
-
-
-
-function searchLyrics() {
-    var artistSearch = document.getElementById("artistSearch").value;
-    document.getElementById("lyrics").textContent = "";
-    $.ajax({
-      type: "GET",
-      data: {
-        apikey: "QTbUwuc4VmQH4fCYBn2UmCYq0CzG9SG8O2VNVfvr",
-        q_artist: artistSearch,
-        format: "json",
-        callback: "json_callback"
-      },
-      url: "https://api.musixmatch.com/ws/1.1/track.search",
-      dataType: "json",
-      jsonCallback: "json_callback",
-      contentType: "application/json",
-      success: function (data) {
-        console.log(data);
-        console.log(data.message.body.track_list[0].track.album_coverart_350x350);
-        console.log(data.message.body.track_list[0].track.lyrics_id);
-        var random = data.message.body.track_list[
-            Math.floor(Math.random() * data.message.body.track_list.length)
-          ];
-        console.log(random.track.track_id);
-        var thisTrack = random.track.track_id;
-  
-        var p = document.createElement("p");
-        p.textContent = thisTrack;
-        p.id = thisTrack;
-  
-        document.getElementById("lyrics").appendChild(p).style.opacity = 0;
-        document.getElementById("ghost").click();
-      },
-      error: function (jqXHR, textStatus, errorTh) {
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorTh);
-      }
-    });
-  }
-  
-  function getLyricsNow() {
-    var trackId = document.getElementById("lyrics").textContent;
-    console.log(trackId);
-    $.ajax({
-      type: "GET",
-      data: {
-        apikey: "QTbUwuc4VmQH4fCYBn2UmCYq0CzG9SG8O2VNVfvr",
-        track_id: trackId,
-        format: "json",
-        callback: "json_callback"
-      },
-      url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get",
-      dataType: "json",
-      jsonCallback: "json_callback",
-      contentType: "application/json",
-      success: function (data) {
-        console.log(data);
-        console.log(data.message.body.lyrics.lyrics_body);
-        var lyricsBody =
-          data.message.body.lyrics.lyrics_body
-            .split(/\s+/)
-            .slice(0, 100)
-            .join(" ") + "...";
-  
-        var j = document.createElement("p");
-        j.textContent = lyricsBody;
-        document.getElementById("lyrics").appendChild(j);
-      },
-      error: function (jqXHR, textStatus, errorTh) {
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorTh);
-      }
-    });
-  }
+$('#search-btn').on('click', function(event){
+    const lyric = $('#search-field').val()
+    $.getJSON(`https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_lyrics=${lyric}&apikey=8c6ba3b63ecfe089c69bf57ce2429746&callback=?`
+    ,function(data) {
+    console.log(data);
+});
+});
