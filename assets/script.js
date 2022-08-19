42884087631ffdacfed9de2c029c451c5d3134ce
Vadim Merenovskiy <101852782+RunninFridge@users.noreply.github.com>
2022-08-19 14:04:30 -0700

    pulled everything together

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,7 @@
+// declaring variables 
 var youTubeBtn = document.getElementById("youtube-btn");
 var appleMusicBtn = document.getElementById("apple-music-btn");
 var spotifyBtn = document.getElementById("spotify-btn");
-
-
 
 //lets you open links in new tabs onclick - vm
 youTubeBtn.onclick = function() {
@@ -17,13 +16,14 @@ spotifyBtn.onclick = function() {
     window.open('https://open.spotify.com/');
 };
 
-
+//on click search btn using api, api key to search for lyrics 
 $('#search-btn').on('click', function(event){
     const lyric = $('#search-field').val()
     $.getJSON(`https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_lyrics=${lyric}&apikey=8c6ba3b63ecfe089c69bf57ce2429746&callback=?`
 , function(data) {  
     console.log(data);
 
+//loop for grabbing the track names and appending them to the ol by creating an li
     var tracklist = data.message.body.track_list;
     for (var i = 0; i < tracklist.length; i++) {
         var tracknameslist = document.createElement("li");
@@ -34,3 +34,11 @@ $('#search-btn').on('click', function(event){
         } 
     })
 });
+
+// list of results set into local storage -ar
+
+var results = document.getElementsByClassName('search-results'); 
+
+localStorage.setItem('results', JSON.stringify(results));
+ 
+var storedResults = JSON.parse(localStorage.getItem('search-results'));
