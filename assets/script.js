79975a2bf085131499f2a837e1ccc1e7c2fa91e6
Stephen Novelli <snovelli1021@gmail.com>
2022-08-18 19:33:31 -0700

    Made changes to HTML id and class names, added JavaScript to return song lyrics

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ const reset = document.getElementById('results');
 
 
 //lets you open links in new tabs onclick - vm
-youTubeBtn.onclick = function() {
+youtubeBtn.onclick = function() {
     window.open('https://youtube.com/');
 };
 
@@ -24,23 +24,12 @@ $('#search-btn').on('click', function(){
 , function(data) {  
     //console.log(data);
 
-//loop for grabbing the track names and appending them to the ol by creating an li
-    var tracklist = data.message.body.track_list;
-    for (var i = 0; i < tracklist.length; i++) {
-        var tracknameslist = document.createElement('li');
-        document.getElementById('results').appendChild(tracknameslist);
-    // console.log(tracklist[i].track.track_name);
-        var tracknames = tracklist[i].track.track_name;
-        tracknameslist.innerHTML = tracknames;
-        }
-        return;
-    })
+
+
+$('#search-btn').on('click', function(event){
+    const lyric = $('#search-field').val()
+    $.getJSON(`https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_lyrics=${lyric}&apikey=8c6ba3b63ecfe089c69bf57ce2429746&callback=?`
+    ,function(data) {
+    console.log(data);
 });
-
-// list of results set into local storage -ar
-
-var results = document.getElementsByClassName('search-results'); 
-
-localStorage.setItem('results', JSON.stringify(results));
- 
-var storedResults = JSON.parse(localStorage.getItem('search-results'));
+});
